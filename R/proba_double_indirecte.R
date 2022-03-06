77039d7ae80d95ee39fb82fc5e9f22d7a35f536c
arnaudfi <arnaud.fizzala@gmail.com>
2022-03-06 15:38:39 +0100

    doc proba_double

--- R/proba_double_indirecte.R
+++ R/proba_double_indirecte.R
@@ -2,14 +2,23 @@
 
 #' Calcul de la probabilité d'inclusion double
 #'
-#' @param liste1
-#' @param liste2
-#' @param ...
+#' @param liste1 vecteur des identifiants des unites de dataPop liées a la premiere unite
+#' @param liste2 vecteur des identifiants des unites de dataPop liées a la seconde unite
+#' @param ... le reste des arguments est identique a la fonction proba_inclusion_indirecte
 #'
 #' @return
 #' @export
 #'
 #' @examples
+#' # calcul de la probabilité d'inclusion double des unités liées c("id1","id6","id12") et c("id2","id5")
+#' proba_double_indirecte(c("id1","id6","id12"),c("id2","id5"),
+#' dataPop = population,
+#' dataStrate =stTest,
+#' identifiant = "id",
+#' nomStrate = "strate",
+#' population = "N",
+#' echantillon =  "n")
+#'
 proba_double_indirecte <- function(liste1,liste2,...){
 
   if (setequal(liste1,liste2)){
